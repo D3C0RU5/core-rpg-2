445eadf170d3ea8f00d540c9605991fa712f6a20
D3C0RU5 <chpeterle.profissional@gmail.com>
2024-11-24 22:53:07 -0300

    feat: created SatusCodeEnum

---
+++ user/src/presentation/helpers/enum/status-code.ts
@@ -0,0 +1,7 @@
+export enum StatusCodeEnum {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNATUTHORIZED = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+}
--- user/src/presentation/helpers/http/http-helper.ts
+++ user/src/presentation/helpers/http/http-helper.ts
@@ -1,22 +1,30 @@
 import { BaseError } from '../../../utils/errors/base-error'
 import { ServerError } from '../../../utils/errors/server-error'
 import { HttpResponse } from '../../protocols/http'
+import { StatusCodeEnum } from '../enum/status-code'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const ok = (data?: any): HttpResponse => ({
-  statusCode: 200,
+  statusCode: StatusCodeEnum.OK,
   body: data,
 })
 
 export const handleError = (error: unknown): HttpResponse => {
+
+  const problemDetails = {
+    url: 'https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1',
+    title: 'An error occurred while processing your requers.',
+    status: StatusCodeEnum.INTERNAL_SERVER_ERROR
+  }
+
   if (error instanceof BaseError) {
     return {
-      statusCode: 400,
+      statusCode: StatusCodeEnum.BAD_REQUEST,
       body: error.toJson(),
     }
   } else {
     return {
-      statusCode: 500,
+      statusCode: StatusCodeEnum.INTERNAL_SERVER_ERROR,
       body: new ServerError(error instanceof Error ? error.stack : undefined),
     }
   }
